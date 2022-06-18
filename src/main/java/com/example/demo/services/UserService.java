e5ed81381068897a4e870552a9cb25431aa9535d
isaac <vi13150@innovaccion.mx>
2022-06-18 16:40:36 -0600

    controller repository and service for user

---
+++ src/main/java/com/example/demo/services/UserService.java
@@ -0,0 +1,23 @@
+package com.example.demo.services;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.models.UserModel;
+import com.example.demo.repositories.UserRepository;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    public ArrayList<UserModel> getUsers() {
+        return (ArrayList<UserModel>) userRepository.findAll();
+    }
+
+    public UserModel saveUser(UserModel user) {
+        return userRepository.save(user);
+    }
+}
